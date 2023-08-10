1af9ca2a5036cd9d2cc5778dae77b88699c00cdd
sssajjad007 <sssajjad.76@gmail.com>
2023-08-10 10:08:31 +0330

    add bootSplash and rnn + vectorIcon

--- android/app/src/main/java/com/rn72_wix/MainActivity.java
+++ android/app/src/main/java/com/rn72_wix/MainActivity.java
@@ -1,32 +1,23 @@
 package com.rn72_wix;
 
-import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import com.reactnativenavigation.NavigationActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.zoontek.rnbootsplash.RNBootSplash;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+  
   @Override
-  protected String getMainComponentName() {
-    return "rn72_wix";
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this);
+    super.onCreate(null);
+    I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+    sharedI18nUtilInstance.allowRTL(getApplicationContext(), false);
+    //  RNBars.init(this, "dark-content");
   }
-
-  /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
-   */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
-  }
+  
 }
