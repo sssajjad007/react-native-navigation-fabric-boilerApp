a5f7c5b604a34b24d6a994db8f9074c0311d506c
sssajjad007 <sssajjad.76@gmail.com>
2023-08-25 16:22:30 +0330

    add firebase notification

---
+++ src/utils/notification.ts
@@ -0,0 +1,90 @@
+/**
+ * remove this line when use
+ */
+export {};
+// import {CustomOmit} from '@common';
+// import messaging, {
+//   FirebaseMessagingTypes,
+// } from '@react-native-firebase/messaging';
+// import {useEffect} from 'react';
+// type CustomOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+// export interface RemoteNotification<T>
+//   extends CustomOmit<FirebaseMessagingTypes.RemoteMessage, 'data'> {
+// //  Nested data from fcm is string. carefully when use
+// //  example data:{ nested:{ a: 1 }}
+// //  => nested will be string
+//   data?: T;
+// }
+
+// export const requestNotificationPermission = async () => {
+//   const authStatus = await messaging().requestPermission();
+//   const enabled =
+//     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+//     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+//   console.log('enabled', enabled);
+//   return enabled;
+// };
+
+// export const getDeviceToken = async () => {
+//   return messaging().getToken();
+// };
+
+// /**
+//  * Notification coming when app in foreground
+//  */
+// export const useInAppNotification = <T = any>(
+//   callback: (remoteNotification: RemoteNotification<T>) => any,
+// ) => {
+//   // effect
+//   useEffect(() => {
+//     messaging().onMessage(
+//       callback as (message: FirebaseMessagingTypes.RemoteMessage) => any,
+//     );
+//   }, []);
+// };
+
+// /**
+//  * Notification coming when app in background or quit state
+//  */
+// export const useBackgroundNotification = <T = any>(
+//   callback: (remoteNotification: RemoteNotification<T>) => any,
+// ) => {
+//   useEffect(() => {
+//     messaging().setBackgroundMessageHandler(
+//       callback as (message: FirebaseMessagingTypes.RemoteMessage) => any,
+//     );
+//   }, []);
+// };
+
+// /**
+//  * User click notification when app in background
+//  */
+// export const useBackgroundOpenedNotification = <T = any>(
+//   callback: (remoteNotification: RemoteNotification<T>) => any,
+// ) => {
+//   // effect
+//   useEffect(() => {
+//     messaging().onNotificationOpenedApp(
+//       callback as (message: FirebaseMessagingTypes.RemoteMessage) => any,
+//     );
+//   }, []);
+// };
+
+// /**
+//  * User click notification when app in killed or quit state
+//  */
+// export const useKilledOpenedNotification = <T = any>(
+//   callback: (remoteNotification: RemoteNotification<T> | null) => any,
+// ) => {
+//   // effect
+//   useEffect(() => {
+//     messaging()
+//       .getInitialNotification()
+//       .then(
+//         callback as (
+//           message: FirebaseMessagingTypes.RemoteMessage | null,
+//         ) => any,
+//       );
+//   }, []);
+// };
